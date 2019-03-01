d716c342ab499e435edee571bd9be13c5265d139
G710 <16511117+G710@users.noreply.github.com>
2019-03-01 11:32:10 +0100

    dunno, im dumb

--- webscraper.py
+++ webscraper.py
@@ -1,37 +1,11 @@
 import os
 
+from lib import tagesschau
+
+# TODO: make this dynamic so contributor don't have to touch this file anymore.
+
 results = []
-
-# walk all files in subdirectory and run the scrape functions
-countries = os.listdir("sources")
-
-for country in countries:
-    sources = os.listdir("sources/" + country)
-    
-    for f in sources:
-        scrape = getattr(__import__("sources/" + country + "/" + f), "scrape")
-        result = scrape()
-        results.append(result)
+results.append(tagesschau.scrape())
 
 
 print(results)
-
-
-
-# data structure
-'''
-scraper_result = {
-    "datetime": "",
-    "country": "",
-    "source": "",
-    articles: [ # should be max 10 articles
-        {
-            "link": ""
-            "headline": "",
-            "infotext": "" # optional
-            "category": ""
-        }
-    ]
-}
-
-'''
