3c446b07fcd180f765dc5a3e43d628268a84dcaa
G710 <16511117+G710@users.noreply.github.com>
2019-03-01 13:00:47 +0100

    added database tinydb

--- webscraper.py
+++ webscraper.py
@@ -1,7 +1,11 @@
-import os
+from tinydb import TinyDB, Query
 
 from lib import tagesschau
 from lib import drdk
+
+
+# setup database
+db = TinyDB('db.json')
 
 # TODO: make this dynamic so contributor don't have to touch this file anymore.
 
@@ -9,4 +13,6 @@ results = []
 results.append(tagesschau.scrape())
 results.append(drdk.scrape())
 
-print(results)
+# add results to database
+for result in results:
+    db.insert(result)
